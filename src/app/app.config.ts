3c3137a3d5d0097909422c3244b256289bf4ebf9
Valeria <ppollux.velas@gmail.com>
2024-06-04 19:25:05 -0600

    si

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,5 +11,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimations(), provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
